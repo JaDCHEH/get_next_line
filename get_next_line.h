84462e7746c8a9a6d35d388850203648ca517ced
Jad Chehboune <cjad@e1r5p9.1337.ma>
2021-11-27 18:19:16 +0100

    Get next line project

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjad <cjad@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 15:37:27 by cjad              #+#    #+#             */
+/*   Updated: 2021/11/27 16:48:15 by cjad             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*ft_strjoin(char	*s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_substr(char	*s, unsigned int start, size_t len);
+char	*ft_strdup(const char	*s1);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cjad <cjad@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 15:37:27 by cjad              #+#    #+#             */
+/*   Updated: 2021/11/27 16:49:34 by cjad             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*ft_strjoin(char	*s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_substr(char	*s, unsigned int start, size_t len);
+char	*ft_strdup(const char	*s1);
+
+#endif
